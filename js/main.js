9f47b7842dc7311dcbb4260e1b35ebb1c3021cc0
Matteo Sarrecchia <morrea@hotmail.it>
2023-04-26 14:34:07 +0200

    password

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, 
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+// definizione nome
+let nome = prompt ("Come ti chiami?");
+console.log(nome);
+
+document.getElementById("nome-id").innerHTML = "Il tuo nome è " + nome;
+
+// definizione colore preferito
+let colorePreferito = prompt ("Qual'è il tuo colore preferito?")
+console.log(colorePreferito)
+
+document.getElementById("colore-id").innerHTML = "Il tuo colore preferito è " + colorePreferito;
+
+// stampa rislultato
+document.getElementById("psw-id").innerHTML = ("La tua password è ") + nome + ("sarrecchia") + colorePreferito + 23;
+
