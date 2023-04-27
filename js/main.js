8bf3276643a789599e397c579812bed631c9c816
Matteo Sarrecchia <morrea@hotmail.it>
2023-04-27 09:21:41 +0200

    agg. cognome

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ console.log(nome);
 
 document.getElementById("nome-id").innerHTML = "Il tuo nome è " + nome;
 
+// definizione cognome
+let cognome = prompt ("Qual'è il tuo cognome?")
+console.log(cognome)
+
+document.getElementById("cognome-id").innerHTML = "Il tuo cognome è " + cognome;
+
 // definizione colore preferito
 let colorePreferito = prompt ("Qual'è il tuo colore preferito?")
 console.log(colorePreferito)
@@ -15,5 +21,5 @@ console.log(colorePreferito)
 document.getElementById("colore-id").innerHTML = "Il tuo colore preferito è " + colorePreferito;
 
 // stampa rislultato
-document.getElementById("psw-id").innerHTML = ("La tua password è ") + nome + ("sarrecchia") + colorePreferito + 23;
+document.getElementById("psw-id").innerHTML = ("La tua password è ") + nome + cognome + colorePreferito + 23;
 
